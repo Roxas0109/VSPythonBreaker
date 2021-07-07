cb0fec60a718063083936fcade5b47d3922ef5bd
Alan <alanhp2@hotmail.com>
2021-07-06 19:10:50 -0700

    created brick class and display them

--- VSPythonBreaker.py
+++ VSPythonBreaker.py
@@ -1,10 +1,10 @@
 import pygame
 import os
+from brick import Brick
 
 #app window size and flags
-flags = pygame.RESIZABLE
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT), flags)
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Python Breaker')
 
 #color
@@ -14,22 +14,38 @@ BLACK = (0,0,0)
 FPS = 60
 
 #CREATE BLOCKS
-BLOCK_WIDTH, BLOCK_HEIGHT = 100, 50
+BLOCK_WIDTH, BLOCK_HEIGHT = 80, 30
 
 BLUE_BLOCK_IMAGE = pygame.image.load(os.path.join('Assets', '01-Breakout-Tiles.png'))
-BLUE_BLOCK = pygame.transform.scale(BLUE_BLOCK_IMAGE, (BLOCK_WIDTH, BLOCK_HEIGHT))
+GREEN_BLOCK_IMAGE = pygame.image.load(os.path.join('Assets', '15-Breakout-Tiles.png'))
+RED_BLOCK_IMAGE = pygame.image.load(os.path.join('Assets', '07-Breakout-Tiles.png'))
 
 def draw_window(blueBlock):
     WIN.fill(BLACK)
-    #pygame.draw.rect(WIN, BLACK, blueBlock)
-    WIN.blit(BLUE_BLOCK,(blueBlock.x,blueBlock.y))
-    WIN.blit(BLUE_BLOCK,(250,300))
-    WIN.blit(BLUE_BLOCK,(400,300))
+    blueBlock.draw(WIN)
     pygame.display.update()
 
+def create_bricks(allBricks):
+    for i in range(7):
+        brick = Brick(BLUE_BLOCK_IMAGE, BLOCK_WIDTH, BLOCK_HEIGHT)
+        brick.rect.x = 60 + i * 100
+        brick.rect.y = 60
+        allBricks.add(brick)
+    for i in range(7):
+        brick = Brick(GREEN_BLOCK_IMAGE, BLOCK_WIDTH, BLOCK_HEIGHT)
+        brick.rect.x = 60 + i * 100
+        brick.rect.y = 100
+        allBricks.add(brick)
+    for i in range(7):
+        brick = Brick(RED_BLOCK_IMAGE, BLOCK_WIDTH, BLOCK_HEIGHT)
+        brick.rect.x = 60 + i * 100
+        brick.rect.y = 140
+        allBricks.add(brick)
+
 def main():
-    #init blocks objects for images
-    blueBlock = pygame.Rect(100,300, BLOCK_WIDTH, BLOCK_HEIGHT)
+    #init blocks
+    allBricks = pygame.sprite.Group()
+    create_bricks(allBricks)
 
     #set FPS
     clock = pygame.time.Clock()
@@ -41,7 +57,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window(blueBlock)
+        draw_window(allBricks)
     pygame.quit()
+
 if __name__ == "__main__":
     main()
